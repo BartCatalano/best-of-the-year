45219c05c93301f085fe5bb9c831dbdb36c78fcd
BartCatalano <catalanobartolomeo@gmail.com>
2025-04-09 12:20:57 +0200

    feat: parte due fatta con un th:each che ittera sulle liste e mi ridà i titoli

--- src/main/java/org/lesson/java/thymeleaf/best_of_the_year/Controller/HomeController.java
+++ src/main/java/org/lesson/java/thymeleaf/best_of_the_year/Controller/HomeController.java
@@ -1,9 +1,16 @@
 package org.lesson.java.thymeleaf.best_of_the_year.Controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.lesson.java.thymeleaf.best_of_the_year.classi.Movie;
+import org.lesson.java.thymeleaf.best_of_the_year.classi.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 // con questa annotazione setto questo file come un controller
 @Controller
@@ -17,6 +24,38 @@ public class HomeController {
         model.addAttribute("name", "Bartolomeo");
         return "home";
     }
+
+    // mwtodi che restituiscono le canzoni e i film
+
+    public List<Movie> getBestMovies() {
+        return Arrays.asList(
+            new Movie(1, "Inception"),
+            new Movie(2, "The Dark Knight"),
+            new Movie(3, "Interstellar")
+        );
+    }
+
+    public List<Song> getBestSongs() {
+        return Arrays.asList(
+            new Song(1, "Iron Man - Black Sabbath"),
+            new Song(2, "Smells Like Teen Spirit - Nirvana"),
+            new Song(3, "Imagine - John Lennon")
+        );
+    }
+
+    @GetMapping("/movies")
+    public String movies(Model model) {
+        List<Movie> movies = getBestMovies();
+        model.addAttribute("movies", movies);
+        return "movies";
+    }
+    
+    @GetMapping("/songs")
+public String songs(Model model) {
+    List<Song> songs = getBestSongs();
+    model.addAttribute("songs", songs);
+    return "songs";
     
     
+}
 }
---
+++ src/main/java/org/lesson/java/thymeleaf/best_of_the_year/classi/Song.java
@@ -0,0 +1,29 @@
+package org.lesson.java.thymeleaf.best_of_the_year.classi;
+
+public class Song {
+   private int id;
+   private String titolo;
+
+
+// creo il costruttore
+  public Song (int id, String titolo){
+    this.id = id;
+    this.titolo = titolo;
+  }
+
+//   creo getter e setter
+
+  public void setTitolo(String titolo){
+    this.titolo = titolo;
+  }
+  public String getTitolo(){
+    return this.titolo;
+  }
+    
+  public void setId(int id){
+    this.id = id;
+  }
+  public int getId(){
+    return this.id;
+  }
+}
